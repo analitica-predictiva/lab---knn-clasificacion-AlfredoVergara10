4dc207c1072036b9f583efe6587ad404914d60ee
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2022-12-05 14:50:54 -0500

    Solución

--- preguntas.py
+++ preguntas.py
@@ -16,15 +16,17 @@ def pregunta_01():
     df = pd.read_csv("house-votes-84.csv", sep=",")
 
     # Cree un vector con la variable de respuesta ('party')
-    y = df["party"].copy()
+    y = df['party'].copy()
 
     # Extraiga las variables de entrada
-    X = df.drop("party", axis=1).values
+    X = df.drop('party', axis=1).values
 
     # Importe el transformador OrdinalEncoder
     from sklearn.preprocessing import OrdinalEncoder
+    OrdinalEncoder = OrdinalEncoder(categories="auto", dtype=np.float64,)
 
     # Transforme las variables de entrada usando fit_transform
+    OrdinalEncoder.fit(X)
     X = OrdinalEncoder().fit_transform(X)
 
     # Importe KNeighborsClassifier de sklearn.neighbors
@@ -32,7 +34,7 @@ def pregunta_01():
     
 
     # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=6)
+    knn = KNeighborsClassifier(n_neighbors=5)
 
     # Entrene el clasificador con el conjunto de entrenamiento
     knn.fit(X, y)
@@ -50,13 +52,14 @@ def pregunta_02():
     df = pd.read_csv("house-votes-84.csv", sep=",")
 
     # Cree un vector con la variable de respuesta ('party')
-    y = df["party"].copy()
+    y = df['party'].copy()
 
     # Extraiga las variables de entrada
-    X = df.drop("party", axis=1).values
+    X = df.drop('party', axis=1).values
 
     # Importe el transformador OrdinalEncoder
     from sklearn.preprocessing import OrdinalEncoder
+    OrdinalEncoder = OrdinalEncoder(categories="auto", dtype=np.float64,)
 
     # Transforme las variables de entrada usando fit_transform
     X = OrdinalEncoder().fit_transform(X)
